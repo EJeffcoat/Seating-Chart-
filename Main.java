456f5bec329d146e91b82683e0f5d08d0d2754dc
Ethan Jeffcoat <ethan.jeffcoat@patelhs.org>
2020-11-09 14:59:09 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    int[] seats = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+    System.out.println("Place your name here");
+    String name = scan.next();
+
+    for (int i = 0; i < 15; i++) {
+      
+    }
+  }
+}
